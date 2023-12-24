cd170e50ef0a8dcecfcfd964ca189707a56d334c
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2023-12-25 00:37:40 +0530

    Update 1.cpp
    Update

--- 1.cpp
+++ 1.cpp
@@ -6,7 +6,7 @@ int main() {
     double a, b, c;
     
     // Input coefficients from the user
-    cout << "Enter the coefficients (a, b, c) of the quadratic equation (ax^2 + bx + c = 0):" << std::endl;
+    cout << "Enter the coefficients (a, b, c) of the quadratic equation (ax^2 + bx + c = 0):" <<endl;
     cout << "a = ";
     cin >> a;
     cout << "b = ";
@@ -21,21 +21,21 @@ int main() {
         // Two real and distinct roots
         double root1 = (-b + sqrt(discriminant)) / (2 * a);
         double root2 = (-b - sqrt(discriminant)) / (2 * a);
-        cout << "Roots are real and distinct:" << std::endl;
-        cout << "Root 1 = " << root1 << std::endl;
-        cout << "Root 2 = " << root2 << std::endl;
+        cout << "Roots are real and distinct:" <<endl;
+        cout << "Root 1 = " << root1 <<endl;
+        cout << "Root 2 = " << root2 <<endl;
     } else if (discriminant == 0) {
         // One real root (repeated)
         double root = -b / (2 * a);
-        cout << "Roots are real and repeated:" << std::endl;
-        cout << "Root 1 = Root 2 = " << root << std::endl;
+        cout << "Roots are real and repeated:" << endl;
+        cout << "Root 1 = Root 2 = " << root << endl;
     } else {
         // Complex roots
         double realPart = -b / (2 * a);
         double imaginaryPart = sqrt(-discriminant) / (2 * a);
-        cout << "Roots are complex and conjugate:" << std::endl;
-        cout << "Root 1 = " << realPart << " + " << imaginaryPart << "i" << std::endl;
-        cout << "Root 2 = " << realPart << " - " << imaginaryPart << "i" << std::endl;
+        cout << "Roots are complex and conjugate:" << endl;
+        cout << "Root 1 = " << realPart << " + " << imaginaryPart << "i" << endl;
+        cout << "Root 2 = " << realPart << " - " << imaginaryPart << "i" << endl;
     }
 
     return 0;
