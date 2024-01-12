4a8002cb5066f8176bb79916dd305a5d000b1108
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-01-13 00:16:39 +0530

    15_Pattern-2.cpp

--- 15_Pattern-2.cpp
+++ 15_Pattern-2.cpp
@@ -1,27 +1,43 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int i, j, k, l;
-    for (i = 1; i <= 4; i++)
-    {
-        for (j = 4; j >= i; j--)
+#include <iostream>  // Include header for input/output operations
+using namespace std;  // Bring the std namespace into scope
+
+int main() {  // Main function where program execution begins
+
+    int i, j, k, l;  // Declare variables for loop counters
+
+    // Outer loop to print the upper half of the diamond
+    for (i = 1; i <= 4; i++) {
+        // Print leading spaces for alignment
+        for (j = 4; j >= i; j--) {
             cout << " ";
-        for (k = i; k <= 2 * i - 1; k++)
+        }
+        // Print increasing numbers
+        for (k = i; k <= 2 * i - 1; k++) {
             cout << k;
-        for (l = 2 * i - 2; l >= i; l--)
+        }
+        // Print decreasing numbers
+        for (l = 2 * i - 2; l >= i; l--) {
             cout << l;
-        cout << endl;
+        }
+        cout << endl;  // Move to the next line
     }
-    for (i = 3; i >= 1; i--)
-    {
-        for (j = 4; j >= i; j--)
+
+    // Outer loop to print the lower half of the diamond
+    for (i = 3; i >= 1; i--) {
+        // Print leading spaces for alignment
+        for (j = 4; j >= i; j--) {
             cout << " ";
-        for (k = i; k <= 2 * i - 1; k++)
+        }
+        // Print increasing numbers
+        for (k = i; k <= 2 * i - 1; k++) {
             cout << k;
-        for (l = 2 * i - 2; l >= i; l--)
+        }
+        // Print decreasing numbers
+        for (l = 2 * i - 2; l >= i; l--) {
             cout << l;
-        cout << endl;
+        }
+        cout << endl;  // Move to the next line
     }
-    return 0;
+
+    return 0;  // Indicate successful program termination
 }
